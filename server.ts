425b3911bd663245b250fce993806f67a7365493
Joseph-kdev <cjkinuthia5@gmail.com>
2026-02-27 19:03:18 +0300

    feat: correct vercel configs to suit Ts

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import app from "./src/index";
 
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT;
 const server = app.listen(port, () => {
   console.log(`Server running at ${port}`);
 });
